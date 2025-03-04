aaa9bfadc94dd319dcb77b50395d07c3634ca259
Nasiru <nasiruadamu214@gmail.com>
2025-03-04 06:53:10 +0100

    Enhance styling of FeaturesSection component; increase heading size and improve feature item layout

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -35,16 +35,17 @@ const FeaturesSection = () => {
   return (
     <section className="py-16 px-6 md:px-12 lg:px-20 bg-gray-100 text-gray-900">
       <div className="max-w-6xl mx-auto ">
-        <h2 className="text-3xl font-bold mb-10">Flexible by design</h2>
+        <h2 className="text-5xl font-bold mb-10">Flexible by design</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {features.map((feature, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center text-center p-6"
-            >
-              <div className="text-5xl mb-4">{feature.icon}</div>
-              <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
-              <p className="text-gray-600">{feature.description}</p>
+            <div key={index} className="flex flex-col items-center  p-6">
+              <div className="text-5xl rounded-xl border-2 p-3 mb-4">
+                {feature.icon}
+              </div>
+              <h3 className="text-3xl font-semibold mb-2">{feature.title}</h3>
+              <p className="text-gray-600 text-xl mt-2">
+                {feature.description}
+              </p>
             </div>
           ))}
         </div>
