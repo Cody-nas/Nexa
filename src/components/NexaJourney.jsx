5fc18e6e5220120ddb3ab95959df72f7c050cf36
Nasiru <nasiruadamu214@gmail.com>
2025-03-04 19:36:26 +0100

    Enhance NexaJourney component layout and styling; update section padding and heading size

--- src/components/NexaJourney.jsx
+++ src/components/NexaJourney.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const NexaJourney = () => {
   return (
-    <section className="flex flex-col">
-      <div>
-        <h1>
-          Start your Nexa <span className="">journey</span>
+    <section className="py-16 px-6 md:px-12 lg:px-20 bg-gray-100  text-gray-900">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-5xl font-bold leading-tight">
+          Start your <span className="text-blue-900">Nexa</span> <br /> journey
         </h1>
       </div>
     </section>
