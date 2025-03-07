031dae0ab84d0bd56369ea8a08c6d2e28a8a61a2
Nasiru <nasiruadamu214@gmail.com>
2025-03-07 13:11:01 +0100

    Add sidebar navigation links to Sidebar component for improved navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,45 @@ const Sidebar = () => {
         <h1 className=" text-2xl text-white font-bold">Admin Dashboard</h1>
       </div>
       <hr />
-      <nav className="mt-3"></nav>
+
+      <ul className="mt-3 text-white font-bold">
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <Link to="/" className="px-3">
+            {/* img */}
+            Dashboard
+          </Link>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <Link to="/" className="px-3">
+            {/* img */}
+            Content
+          </Link>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <Link to="/" className="px-3">
+            {/* img */}
+            Tutorial
+          </Link>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <Link to="/" className="px-3">
+            {/* img */}
+            Comments
+          </Link>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <Link to="/" className="px-3">
+            {/* img */}
+            Template & Themes
+          </Link>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <Link to="/" className="px-3">
+            {/* img */}
+            Media
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
